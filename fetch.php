6e5abd201abd63c00996520968ff06ad0a542750
Raquibul Islam <ranacse05@gmail.com>
2022-03-21 20:08:26 +0600

    Outdoor temp added

--- fetch.php
+++ fetch.php
@@ -2,14 +2,14 @@
 
 require_once 'db.php';
 
-  $results= $db->query("select minutes,temp,time_stamp from minutes order by minutes asc");
+  $results= $db->query("select minutes,temp,time_stamp,outdoor from minutes order by minutes asc");
 
   //$results= $db->query("select avg(temp) from minutes");
 
   //print_r($results);
   //exit();
   $data= array();
-
+  $outdoor = array(); 
 
   while ($res= $results->fetchArray())
   {
@@ -17,10 +17,19 @@ require_once 'db.php';
     $temp = array();
     $temp[]  = $res['minutes'];
     $temp[] = $res['temp'];
-   
     array_push($data, $temp);    
   }
-echo json_encode($data);
+
+  while ($res= $results->fetchArray())
+  {
+    //print_r($res);
+    $temp = array();
+    $temp[]  = $res['minutes'];
+    $temp[] = $res['outdoor'];
+    array_push($outdoor, $temp);    
+  }
+
+echo json_encode(array('indoor'=>$data,'outdoor'=>$outdoor));
 
 
    $db->close();
--- monitor.php
+++ monitor.php
@@ -4,11 +4,34 @@
    if(!$file = fopen("sys.log","a+"))
    $file = fopen("sys.log","w");
   	
-	
-	$temp = $_GET['temperature'];
+
+   
+  //https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/23.830931%2C%2090.417709?unitGroup=metric&key=Z6SNVEG7JTYQU52LBQULPV3LJ&contentType=json
+
+	$temp = number_format($_GET['temperature'],2);
 	$minute =  intval(date('i'));
 	$time = time();
    $server = $_GET['server'];
+
+
+   if($minute==0)
+   {
+      $url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/23.7740004%2C%2090.4276922?/2022-03-17T".date('H').":00:00&unitGroup=metric&key=Z6SNVEG7JTYQU52LBQULPV3LJ&contentType=json&include=current";
+      $data = file_get_contents($url);
+      
+      $raw = json_decode($data);
+      // echo 'DateTime:'.print_r($raw->days[0]->datetime);
+      
+      //echo '<pre>';
+      //print_r($raw->currentConditions->temp);
+      //echo '</pre>';
+      $sql = 'UPDATE minutes set outdoor="'.$raw->currentConditions->temp.'"';
+      $ret = $db->exec($sql);
+      $sql = 'UPDATE minutes2 set outdoor="'.$raw->currentConditions->temp.'"';
+      $ret = $db->exec($sql);
+
+   }
+
 
     $sql = 'UPDATE minutes set time_stamp="'.$time.'", temp="'.$temp.'" where minutes="'.$minute.'"';
 	
